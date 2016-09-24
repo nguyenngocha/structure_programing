3741b65e6ca8e0771be8c8d7314d530f161cbff3
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-09-24 03:30:35 +0000

    init gem devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_one :address
 
   has_many :carts
+  
+  accepts_nested_attributes_for :address, allow_destroy: true
 end
